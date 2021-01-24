b614dacbd2540abfd087e3790082b990495a34ad
William Barnes <jakebarnes1989@gmail.com>
2021-01-24 15:11:23 -0500

    Projectiles, Animals, and Boundaries (#2)
    * Player Controller
    
    Implemented PlayerController script.
    - Horizontal Movement
    - Boundary Restraints
    
    * Update PlayerController.cs
    
    - Refactored bounds into single variable for efficiency and cleanliness.
    
    * Update PlayerController.cs
    
    Added additional bounds comments for clarity.
    
    * Projectiles and Animals
    
    - Implemented animal movement and prefabs
    - Implemented projectile movement, spawning, and prefab.
    
    * DestroyOutOfBounds
    
    - Implemented script to destroy objects once they're out of bounds.
    - Added DOOB script to animals and projectile prefabs.
    - Adjusted player controllers horizontal bounds to insure player remains on screen in 16:9

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,14 @@ public class PlayerController : MonoBehaviour
 
     // Initialize Variables
     public GameObject player;
+    public GameObject projectilePrefab;
+
     public float horizontalInput = 0.0f;
     public float speed = 10.0f;
-    private int bounds = 20;
+
+    private int bounds = 15;
+
+    public int isFire = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +28,15 @@ public class PlayerController : MonoBehaviour
         // Get player input
         horizontalInput = Input.GetAxis("Horizontal");
 
+        // Move Player
+        MovePlayer();
+
+        FireProjectile();
+    }
+
+    void MovePlayer()
+    {
+
         // Move player left/right, includes speed.
         player.transform.position += (Vector3.right * horizontalInput * speed * Time.deltaTime);
 
@@ -31,10 +45,24 @@ public class PlayerController : MonoBehaviour
         {
             // Set player position to left bounds
             player.transform.position = new Vector3(-bounds, transform.position.y, transform.position.z);
-        } else if (player.transform.position.x > bounds)
+        }
+        else if (player.transform.position.x > bounds)
+
         {
             // Set player position to right bounds
             player.transform.position = new Vector3(bounds, transform.position.y, transform.position.z);
         }
+
+    }
+
+    void FireProjectile()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate a projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            isFire = 1;
+        }
+
     }
 }
