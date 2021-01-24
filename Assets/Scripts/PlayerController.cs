be3b5719995069006d9e47a2832542d983b14229
William Barnes <jakebarnes1989@gmail.com>
2021-01-23 21:59:54 -0500

    Player Controller (#1)
    * Player Controller
    
    Implemented PlayerController script.
    - Horizontal Movement
    - Boundary Restraints
    
    * Update PlayerController.cs
    
    - Refactored bounds into single variable for efficiency and cleanliness.
    
    * Update PlayerController.cs
    
    - Added additional bounds comments for clarity.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Initialize Variables
+    public GameObject player;
+    public float horizontalInput = 0.0f;
+    public float speed = 10.0f;
+    private int bounds = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move player left/right, includes speed.
+        player.transform.position += (Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        // Restrict player to world bounds
+        if (player.transform.position.x < -bounds)
+        {
+            // Set player position to left bounds
+            player.transform.position = new Vector3(-bounds, transform.position.y, transform.position.z);
+        } else if (player.transform.position.x > bounds)
+        {
+            // Set player position to right bounds
+            player.transform.position = new Vector3(bounds, transform.position.y, transform.position.z);
+        }
+    }
+}
